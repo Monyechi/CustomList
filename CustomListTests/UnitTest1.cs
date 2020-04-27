fe66bee58a6031d020f02bcb83c854265ed76107
Monyechi <monyechi@gmail.com>
2020-04-27 16:19:14 -0400

    first attempt at add methods

--- CustomListTests/UnitTest1.cs
+++ CustomListTests/UnitTest1.cs
@@ -40,10 +40,42 @@ namespace CustomListTests
             // assert
             Assert.AreEqual(expected, actual);
         }
-        
+        [TestMethod]
+        public void Add_AddingToACustomListThatHasSomeValues_CountOfCustomListIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddingMoreItemsThanCapacityOfTheCustomList_CapacityOfTheCustomListIncrements()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int expected = 10;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
         // what happens if you add multiple things (or add to a CustomList that already has some values)?
-            // what happens to the last-added item?
-            // what happens to the Count?
+        // what happens to the last-added item?
+        // what happens to the Count?
 
         // what happens if you add more items than the initial Capacity of the CustomList?
 
