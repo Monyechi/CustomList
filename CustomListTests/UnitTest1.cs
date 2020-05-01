fa1c4c7517b7b729324ced0444eb184ec59128ef
Monyechi <monyechi@gmail.com>
2020-05-01 08:10:00 -0400

    Not sure the last thing I updated

--- CustomListTests/UnitTest1.cs
+++ CustomListTests/UnitTest1.cs
@@ -59,19 +59,49 @@ namespace CustomListTests
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void Add_AddingMoreItemsThanCapacityOfTheCustomList_CapacityOfTheCustomListIncrements()
+        public void Add_AddingMoreItemsThanCapacityOfTheCustomList_CapacityOfTheCustomListIncrementsByFour()
         {
             // arrange
             CustomList<int> testList = new CustomList<int>();
             int itemToAdd = 10;
             int itemToAddTwo = 15;
-            int expected = 15;
+            int itemToAddThree = 11;
+            int itemToAddFour = 5;
+            int itemToAddFive = 18;
+            int expected = 8;
             int actual;
 
             // act
             testList.Add(itemToAdd);
             testList.Add(itemToAddTwo);
-            actual = testList[1];
+            testList.Add(itemToAddThree);
+            testList.Add(itemToAddFour);
+            testList.Add(itemToAddFive);
+            actual = testList.Capacity;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Add_AddingFiveItemsThanCapacityOfTheCustomList_CheckCapacity()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd = 10;
+            int itemToAddTwo = 15;
+            int itemToAddThree = 11;
+            int itemToAddFour = 5;
+            int itemToAddFive = 18;
+            int expected = 8;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd);
+            testList.Add(itemToAddTwo);
+            testList.Add(itemToAddThree);
+            testList.Add(itemToAddFour);
+            testList.Add(itemToAddFive);
+            actual = testList.Capacity;
 
             // assert
             Assert.AreEqual(expected, actual);
@@ -82,6 +112,146 @@ namespace CustomListTests
         // what happens to the Count?
 
         // what happens if you add more items than the initial Capacity of the CustomList?
+        [TestMethod]
+        public void Remove_RemovingDuplicateValuesFromCustomList_ValueOfTwoIndexStaysTheSame()
+        {
+            // arrange
+            CustomList<string> testList = new CustomList<string>(); // Creating a list that holds strings
+
+            string expected = "Earth";
+            string actual;
+
+            // act
+            testList.Add("Sun");		// Adding the value "Sun"
+            testList.Add("Moon");		// Adding the value "Moon"
+            testList.Add("Earth");		// Adding the value "Earth"
+            testList.Add("Moon");       // Adding the value "Moon" for the second time
+
+            // Currently our list holds the values: "Sun", "Moon", "Earth", "Moon"
+            // Currently our list has a count of: 4
+
+            testList.Remove("Moon");    // Calling the Remove method to remove the first "Moon" in the list
+
+            // Assuming the remove method worked we expect:
+            // Our list to now hold the values: "Sun", "Earth", "Moon"
+            // Our list to now have a count of: 3
+
+            actual = testList[2]; // Grabbing the value at index 2, which now should be "Moon"
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemovingTenValueFromCustomList_CountBecomesOne()
+        {
+            
+            CustomList<string> testList = new CustomList<string>(); 
+
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add("0");
+            testList.Add("1");
+            testList.Add("2");
+            testList.Add("3");		
+            testList.Add("4");		
+            testList.Add("5");		
+            testList.Add("6");		
+            testList.Add("7");		
+            testList.Add("8");		
+            testList.Add("9");		
+            testList.Add("10");
+
+
+            testList.Remove("0");
+            testList.Remove("2");
+            testList.Remove("3");
+            testList.Remove("4");
+            testList.Remove("5");
+            testList.Remove("6");
+            testList.Remove("7");
+            testList.Remove("8");
+            testList.Remove("9");
+            testList.Remove("10");
+
+            actual = testList.Count; 
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemovingAllValuesFromCustomList_CustomListCountGoesToZero()
+        {
+            
+            CustomList<string> testList = new CustomList<string>(); 
+
+            int expected = 0;
+            int actual;
+
+            // act
+            testList.Add("Sun");		
+            testList.Add("Moon");		
+            testList.Add("Earth");		
+            testList.Add("Moon");                      
+
+            testList.Remove("Sun");    
+            testList.Remove("Moon");    
+            testList.Remove("Earth");    
+            testList.Remove("Moon");    
+          
+            actual = testList.Count; 
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemovingAllValuesFromCustomListExceptOne_CountBecomesOne()
+        {
+            
+            CustomList<string> testList = new CustomList<string>(); 
+
+            int expected = 1;
+            int actual;
+
+            // act
+            testList.Add("Sun");		
+            testList.Add("Moon");		
+            testList.Add("Earth");		
+            testList.Add("Moon");                      
+
+            testList.Remove("Moon");    
+            testList.Remove("Earth");
+            testList.Remove("Moon");
+
+            actual = testList.Count; 
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_RemovingTwoValuesFromCustomList_CountBecomesTwo()
+        {
+            
+            CustomList<string> testList = new CustomList<string>(); 
+
+            int expected = 2;
+            int actual;
+
+            // act
+            testList.Add("Sun");		
+            testList.Add("Moon");		
+            testList.Add("Earth");		
+            testList.Add("Moon");                      
+
+            testList.Remove("Sun");    
+            testList.Remove("Moon");    
+          
+            actual = testList.Count; 
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }
--- Sandbox/CustomList.cs
+++ Sandbox/CustomList.cs
@@ -8,19 +8,102 @@ namespace Sandbox
 {
     public class CustomList<T>
     {
-        // member variables (HAS A)
-        private T[] items;
 
-        // constructor (SPAWNER)
+        private T[] items;
+        private int count;
+
+        private int capacity;
+
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < count && index >= 0)
+                {
+                    return items[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
         public CustomList()
         {
-            items = new T[4];
+            count = 0;
+            capacity = 4;
+            items = new T[capacity];
         }
 
-        // member methods (CAN DO)
+
+        //Check if we have reached max capacity
+        //create a temp array to hold the original values
+        //double the size of our items array
+        //using a for loop, pass each value from out temp array into our new larger items array
         public void Add(T item)
         {
+            T[] tempArray;
+            
+            items[count] = item;
+            count++;
+            if (count == capacity)
+            {
+                tempArray = items;
+                capacity = capacity * 2;
+                items = new T[capacity];
+                for (int i = 0; i < count; i++)
+                {
+                    items[i] = tempArray[i];
+                }
+            }           
 
         }
+        //Capacity = 4
+        //Count = 0
+        //null null null null
+        //Add(23)
+        //Add(56)
+        //Add(87)
+        //23 56 87 null
+        //Add(2)
+        //23 56 87 2
+        //Count = 4
+        //Capacity = 4
+        //increase the size of our items by double
+        //After we increase capacity...
+        //23 56 87 2 null null null null
+        //Add(30)
+        //23 56 87 2 30 null null null
+
+
+        public void Remove(T item)
+        {
+            items[count] = item;
+            count--;
+        }
+        
     }
 }
